1b4c23dac45f0a6d00e2524037a67fa8ba39a31f
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-10 12:27:04 +0100

    Implement saving and loading of local user data

---
+++ src/apis/user/user.js
@@ -0,0 +1,50 @@
+import localforage from 'localforage';
+
+async function get(key) {
+  const item = await localforage.getItem(key);
+  return item;
+}
+
+function set(key, item) {
+  return localforage.setItem(key, item);
+}
+
+export async function createUser() {
+  // user.createdAt date is turned into a string
+  const user = {
+    id: Math.random().toString(36).substring(2, 9),
+    createdAt: new Date(Date.now()),
+    basket: [],
+  };
+  await set('user', user);
+  return user;
+}
+export async function getUser() {
+  const user = await get('user');
+  // When createdAt date/time is stored to disk, it is stringified. Turn back into a Date
+  return { ...user, createdAt: new Date(user.createdAt) };
+}
+
+export async function addToBasket(item) {
+  const user = await getUser();
+  await set('user', {
+    ...user,
+    basket: [...user.basket, item],
+  });
+}
+
+export async function removeFromBasket(item) {
+  const user = await getUser();
+  await set('user', {
+    ...user,
+    basket: user.basket.filter((i) => i !== item),
+  });
+}
+
+export async function clearBasket() {
+  const user = await getUser();
+  await set('user', {
+    ...user,
+    basket: [],
+  });
+}
---
+++ src/apis/user/user.test.js
@@ -0,0 +1,150 @@
+import {
+  createUser,
+  getUser,
+  addToBasket,
+  removeFromBasket,
+  clearBasket,
+} from './user';
+import { afterEach, beforeEach, expect, vi } from 'vitest';
+
+import localforage from 'localforage';
+vi.mock('localforage');
+
+let data;
+beforeEach(() => {
+  data = {};
+
+  localforage.setItem.mockImplementation(async (key, item) => {
+    data[key] = item;
+  });
+
+  localforage.getItem.mockImplementation(async (key) => {
+    return data[key];
+  });
+});
+
+afterEach(() => {
+  vi.resetAllMocks();
+});
+
+afterAll(() => {
+  vi.restoreAllMocks();
+});
+
+const product1 = {
+  id: 0,
+  title: 0,
+  price: 0,
+  description: 0,
+  category: 0,
+  image: 0,
+  rating: {
+    rate: 0,
+    count: 0,
+  },
+};
+
+const product2 = {
+  id: 1,
+  title: 0,
+  price: 0,
+  description: 0,
+  category: 0,
+  image: 0,
+  rating: {
+    rate: 0,
+    count: 0,
+  },
+};
+
+describe('User API', () => {
+  describe('User object', () => {
+    it('is created with the correct properties', async () => {
+      const user = await createUser();
+      expect(user.id).toBeDefined();
+      expect(user.createdAt).toBeDefined();
+      expect(user.basket).toBeDefined();
+    });
+
+    it('id property is of type number', async () => {
+      const user = await createUser();
+      expect(typeof user.id).toBe('string');
+    });
+
+    it('createdAt property is an instance of Date', async () => {
+      const user = await createUser();
+      expect(user.createdAt instanceof Date).toBe(true);
+    });
+
+    it('basket is an array', async () => {
+      const user = await createUser();
+      expect(Array.isArray(user.basket)).toBe(true);
+    });
+  });
+
+  it('saves user data', async () => {
+    const createdUser = await createUser();
+    expect(data).toEqual({ user: createdUser });
+  });
+
+  it('loads user data', async () => {
+    data = {
+      user: {
+        id: 0,
+        createdAt: new Date(Date.now()),
+        basket: [],
+      },
+    };
+
+    const user = await getUser();
+    expect(user).toEqual(data.user);
+  });
+
+  it('adds item to basket', async () => {
+    data = {
+      user: {
+        id: 0,
+        createdAt: new Date(Date.now()),
+        basket: [],
+      },
+    };
+
+    await addToBasket(product1);
+    await addToBasket(product2);
+    const basket = data.user.basket;
+    expect(basket.length).toBe(2);
+    expect(basket[0]).toBe(product1);
+    expect(basket[1]).toBe(product2);
+  });
+
+  it('removes item from basket', async () => {
+    data = {
+      user: {
+        id: 0,
+        createdAt: new Date(Date.now()),
+        basket: [product1, product2],
+      },
+    };
+
+    await removeFromBasket(product1);
+    const basket = data.user.basket;
+    expect(basket.length).toBe(1);
+    expect(basket[0]).toBe(product2);
+
+    await removeFromBasket(product2);
+    expect(data.user.basket.length).toBe(0);
+  });
+
+  it("clearBasket removes all items from user's basket", async () => {
+    data = {
+      user: {
+        id: 0,
+        createdAt: new Date(Date.now()),
+        basket: [product1, product2],
+      },
+    };
+
+    await clearBasket();
+    expect(data.user.basket.length).toBe(0);
+  });
+});
