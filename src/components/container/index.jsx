f3db67b9770e3abbbeeb06ae945455b6773f471f
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-11 16:25:39 +0100

    Add vertical padding to Container component

--- src/components/container/index.jsx
+++ src/components/container/index.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const ContainerStyled = styled.div`
   max-width: 1280px;
   margin: 0 auto;
+  padding: 1rem 0;
 `;
 
 export default function Container({ children, as = 'div' }) {
