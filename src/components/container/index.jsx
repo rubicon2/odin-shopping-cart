d0c9f018bc4c6d3c9ef32294560b704fc5796ad4
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-11 15:27:06 +0100

    Modify Container component so style can be extended

--- src/components/container/index.jsx
+++ src/components/container/index.jsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
-const Div = styled.div`
+export const ContainerStyled = styled.div`
   max-width: 1280px;
   margin: 0 auto;
 `;
 
 export default function Container({ children, as = 'div' }) {
-  return <Div as={as}>{children}</Div>;
+  return <ContainerStyled as={as}>{children}</ContainerStyled>;
 }
