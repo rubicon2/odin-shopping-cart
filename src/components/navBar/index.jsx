f2daad775a0fd38c5888da100f88b315e48eea2f
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-11 15:27:44 +0100

    Implement NavBar component

---
+++ src/components/navBar/index.jsx
@@ -0,0 +1,67 @@
+import NavBasket from '../navBasket';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavList = styled.ul`
+  list-style: none;
+  padding: 0;
+  display: flex;
+  align-items: center;
+
+  & > * {
+    /* Display block so we can have some nice vertical padding */
+    display: block;
+    padding: 0.75rem 2rem;
+
+    &:last-child {
+      padding-right: 0;
+    }
+  }
+`;
+
+const NavLinkStyled = styled(NavLink)`
+  font-size: 1.3rem;
+  font-weight: 700;
+  text-decoration: none;
+  color: var(--color--dark);
+
+  &.active {
+    color: red;
+  }
+
+  &.pending {
+    color: pink;
+  }
+`;
+
+export default function NavBar() {
+  return (
+    <nav>
+      <NavList>
+        <li>
+          <NavLinkStyled
+            to="/"
+            className={(isActive, isPending) =>
+              isActive ? 'active' : isPending ? 'pending' : ''
+            }
+          >
+            Home
+          </NavLinkStyled>
+        </li>
+        <li>
+          <NavLinkStyled
+            to="/shop"
+            className={(isActive, isPending) =>
+              isActive ? 'active' : isPending ? 'pending' : ''
+            }
+          >
+            Shop
+          </NavLinkStyled>
+        </li>
+        <li>
+          <NavBasket />
+        </li>
+      </NavList>
+    </nav>
+  );
+}
---
+++ src/components/navBar/index.test.jsx
@@ -0,0 +1,5 @@
+describe('NavBar', () => {
+  it.skip('Exists', () => {
+    // Whatever
+  });
+});
