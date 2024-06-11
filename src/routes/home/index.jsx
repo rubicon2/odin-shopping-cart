480a9926541c0bf0598b1c0e6b514938dd538c71
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-11 15:33:12 +0100

    Add placeholder content to Home route

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -4,6 +4,14 @@ export default function Home() {
   return (
     <Container as="main">
       <h2>Home</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum ea atque,
+        at fugit vel obcaecati excepturi. Vel dignissimos natus tenetur!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+        ducimus sed laboriosam consectetur! Ab, maiores?
+      </p>
     </Container>
   );
 }
