df15b83ad2205367d6c553a4ed2c5f77e1cf38ed
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-12 15:49:18 +0100

    Implement basic ProductFilter component
    Still need to implement filters for category
    and rating.

---
+++ src/components/productFilter/index.jsx
@@ -0,0 +1,68 @@
+import PriceFilter from '../priceFilter';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.aside`
+  width: 600px;
+  border: 2px solid var(--color--light);
+  border-radius: 10px;
+  padding: 1rem;
+  box-shadow: 5px 5px 5px 5px var(--color--shadow);
+  height: min-content;
+`;
+
+function getLowestPrice(products) {
+  return products
+    .map((product) => product.price)
+    .reduce((min, current) => (current < min ? current : min));
+}
+
+function getHighestPrice(products) {
+  return products
+    .map((product) => product.price)
+    .reduce((max, current) => (current > max ? current : max));
+}
+
+function getCategories(products) {
+  return Array.from(new Set(products.map((product) => product.category)));
+}
+
+export default function ProductFilter({ products, onChange }) {
+  const minPrice = getLowestPrice(products);
+  const maxPrice = getHighestPrice(products);
+  const categories = getCategories(products);
+
+  const [query, setQuery] = useState({
+    selectedMin: minPrice,
+    selectedMax: maxPrice,
+    selectedCategories: categories,
+    selectedRating: 0,
+  });
+
+  const handleQueryChange = (queryObj) => {
+    const newQuery = { ...query, ...queryObj };
+    const visibleProductIds = products
+      .filter(
+        (product) =>
+          product.price >= newQuery.selectedMin &&
+          product.price <= newQuery.selectedMax &&
+          newQuery.selectedCategories.includes(product.category) &&
+          product.rating.rate >= newQuery.selectedRating,
+      )
+      .map((product) => product.id);
+    onChange(visibleProductIds);
+    setQuery(newQuery);
+  };
+
+  // category - range of tick boxes
+  // rating.rate - range of tick boxes
+  return (
+    <Container>
+      <PriceFilter
+        minPrice={minPrice}
+        maxPrice={maxPrice}
+        onChange={handleQueryChange}
+      />
+    </Container>
+  );
+}
