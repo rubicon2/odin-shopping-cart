641e76d82198ff170a49a25237b902d270f5b126
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-17 11:02:10 +0100

    Add ProductFilter and ProductList components to shop route

--- src/routes/shop/index.jsx
+++ src/routes/shop/index.jsx
@@ -1,7 +1,22 @@
-import { getProducts } from '../../apis/products/products';
 import Container from '../../components/container';
 import PageHeading from '../../components/pageHeading';
+import ProductFilter from '../../components/productFilter';
+import ProductList from '../../components/productList';
+import { getProducts } from '../../apis/products/products';
 import { useLoaderData } from 'react-router';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const ShopContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  gap: 2rem;
+`;
+
+const StickyFilter = styled(ProductFilter)`
+  position: sticky;
+  top: 1rem;
+`;
 
 export async function loader() {
   const products = await getProducts();
@@ -9,14 +24,25 @@ export async function loader() {
 }
 
 export default function Shop() {
-  const products = useLoaderData();
+  const productList = useLoaderData();
+
+  const [visibleIds, setVisibleIds] = useState(
+    productList.map((product) => product.id),
+  );
+
+  const visibleProducts = productList.filter((product) =>
+    visibleIds.includes(product.id),
+  );
 
   return (
     <Container as="main">
       <PageHeading>Shop</PageHeading>
-      {products.map((product) => (
-        <h3 key={product.id}>{product.title}</h3>
-      ))}
+      {productList && (
+        <ShopContainer>
+          <StickyFilter products={productList} onChange={setVisibleIds} />
+          <ProductList products={visibleProducts} />
+        </ShopContainer>
+      )}
     </Container>
   );
 }
