58f287d9576cd293daba63f04bca99a48751af01
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-18 14:15:33 +0100

    navBasket: show how many items user has in basket

--- src/components/navBasket/index.jsx
+++ src/components/navBasket/index.jsx
@@ -1,21 +1,61 @@
 import BasketIcon from '/basket.svg';
 import BasketIconRed from '/basket-red.svg';
-import { useLocation, Link } from 'react-router-dom';
+import { useLocation, Link, useLoaderData } from 'react-router-dom';
+import styled from 'styled-components';
 
 // Preload icon so there is no delay when user clicks link to /basket
 // Pretty hacky... is there any other way of doing this?
 // const red = new Image();
 // red.src = BasketIconRed;
 
+const BasketLink = styled(Link)`
+  position: relative;
+
+  img {
+    width: 100%;
+  }
+`;
+
+const ImgContainer = styled.div`
+  width: 50px;
+  height: 50px;
+`;
+
+const ItemCount = styled.div`
+  position: absolute;
+  padding: 0.1em 0.5em;
+  border-radius: 10px;
+  background-color: var(--color--dark);
+  color: white;
+  font-weight: 500;
+  top: 50px;
+  left: 25px;
+
+  &.active {
+    background-color: var(--accent-color);
+  }
+`;
+
 export default function NavBasket() {
+  const user = useLoaderData();
+  const basketItemCount = user.basket.length;
+
   const location = useLocation();
   const isActive = location.pathname === '/basket';
   return (
-    <Link to="/basket">
-      <img
-        src={isActive ? BasketIconRed : BasketIcon}
-        alt="x items in basket"
-      />
-    </Link>
+    <BasketLink to="/basket">
+      <ImgContainer>
+        <img
+          src={isActive ? BasketIconRed : BasketIcon}
+          alt={`${basketItemCount} items in basket`}
+          title={`${basketItemCount} items in basket`}
+        />
+      </ImgContainer>
+      {basketItemCount > 0 && (
+        <ItemCount className={isActive ? 'active' : ''}>
+          {basketItemCount}
+        </ItemCount>
+      )}
+    </BasketLink>
   );
 }
