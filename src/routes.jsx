70eae2c9066ea6e25d4d7d3c1998902e0facf5ea
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-18 09:59:27 +0100

    getUser in root loader so user data is available to child navBasket

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,14 @@
 import Basket from './routes/basket';
 import Shop, { loader as ShopLoader } from './routes/shop';
 import Home from './routes/home';
-import Root from './routes/root';
+import Root, { loader as RootLoader } from './routes/root';
 import Error from './routes/error';
 
 export const routes = [
   {
     path: '/',
     element: <Root />,
+    loader: RootLoader,
     children: [
       {
         errorElement: <Error />,
--- src/routes/root/index.jsx
+++ src/routes/root/index.jsx
@@ -1,5 +1,11 @@
 import Header from '../../components/header';
+import { getUser } from '../../apis/user/user';
 import { Outlet } from 'react-router';
+
+export async function loader() {
+  let user = await getUser();
+  return user;
+}
 
 export default function Root() {
   return (
