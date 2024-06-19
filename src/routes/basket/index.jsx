f5617a7fc02255c59264c76becac6371113ae59b
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-19 10:26:49 +0100

    Add clear basket button to basket route for testing navBasket

--- src/routes/basket/index.jsx
+++ src/routes/basket/index.jsx
@@ -1,10 +1,15 @@
 import Container from '../../components/container';
 import PageHeading from '../../components/pageHeading';
+import { clearBasket } from '../../apis/user/user';
+import styled from 'styled-components';
+
+const ClearBasketButton = styled.button``;
 
 export default function Basket() {
   return (
     <Container as="main">
       <PageHeading>Basket</PageHeading>
+      <ClearBasketButton onClick={() => clearBasket()}>Clear</ClearBasketButton>
     </Container>
   );
 }
