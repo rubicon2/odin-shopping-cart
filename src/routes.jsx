c0db20a4f12dd821d26f3c89db6ae5a15502f051
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-19 15:50:39 +0100

    Remove redundant loader from root route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,13 @@
 import Basket from './routes/basket';
 import Shop, { loader as ShopLoader } from './routes/shop';
 import Home from './routes/home';
-import Root, { loader as RootLoader } from './routes/root';
+import Root from './routes/root';
 import Error from './routes/error';
 
 export const routes = [
   {
     path: '/',
     element: <Root />,
-    loader: RootLoader,
     children: [
       {
         errorElement: <Error />,
--- src/routes/root/index.jsx
+++ src/routes/root/index.jsx
@@ -1,11 +1,5 @@
 import Header from '../../components/header';
-import { getUser } from '../../apis/user/user';
 import { Outlet } from 'react-router';
-
-export async function loader() {
-  let user = await getUser();
-  return user;
-}
 
 export default function Root() {
   return (
