bba4cf290c285f3b74d7014cc2a693b029fe737d
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-20 15:38:12 +0100

    Add horizontal padding to Container component so looks better on small screens

--- src/components/container/index.jsx
+++ src/components/container/index.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const ContainerStyled = styled.div`
   max-width: 1280px;
   margin: 0 auto;
-  padding: 1rem 0;
+  padding: 1rem;
 `;
 
 export default function Container({ children, as = 'div', className = '' }) {
