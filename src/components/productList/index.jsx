2a1a6ab1d1327cbf07c2a29d7c9d280f2a3db0d8
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-24 16:51:39 +0100

    ProductList: remove redundant css that appears to do nothing

--- src/components/productList/index.jsx
+++ src/components/productList/index.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 
 const Container = styled.main`
   display: grid;
-  grid-auto-rows: max-content;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   gap: 1.5rem;
 `;
