f5e84f57355c744ddec6ab2e1d8837116468deec
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-25 12:03:13 +0100

    Implement sticky header nav bar

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,7 +4,21 @@ import LogoDark from '/mega-shop-logo-dark.svg';
 import styled from 'styled-components';
 
 const Background = styled.header`
+  background-color: white;
+  position: relative;
+  z-index: 2;
+
+  @media (min-width: 859px) {
+    display: none;
+  }
+`;
+
+const StickyBackground = styled.header`
+  background-color: white;
   box-shadow: 0px 5px 20px 5px var(--color--shadow);
+  position: sticky;
+  top: 0;
+  z-index: 1;
 `;
 
 const Content = styled(Container)`
@@ -24,13 +38,26 @@ const Logo = styled.img`
   padding: min(5vw, 1.5rem) 0;
 `;
 
+const StickyLogo = styled(Logo)`
+  @media (max-width: 860px) {
+    display: none;
+  }
+`;
+
 export default function Header() {
   return (
-    <Background>
-      <Content>
-        <Logo src={LogoDark} alt="" />
-        <NavBar />
-      </Content>
-    </Background>
+    <>
+      <Background>
+        <Content>
+          <Logo src={LogoDark} alt="" />
+        </Content>
+      </Background>
+      <StickyBackground>
+        <Content>
+          <StickyLogo src={LogoDark} alt="" />
+          <NavBar />
+        </Content>
+      </StickyBackground>
+    </>
   );
 }
