31a4e1ad54bc215224e8eca0d95c18855a2a1bef
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-25 16:12:57 +0100

    Shop route: fix broken sticky filter layout in small viewports

--- src/routes/shop/index.jsx
+++ src/routes/shop/index.jsx
@@ -8,14 +8,21 @@ import styled from 'styled-components';
 
 const ShopGrid = styled.div`
   display: grid;
-  grid-template-columns: 1fr 3fr;
   gap: 2rem;
+
+  @media (min-width: 860px) {
+    grid-template-columns: 1fr 3fr;
+  }
 `;
 
 const StickyFilter = styled(ProductFilter)`
-  position: sticky;
-  /* Height of Header component plus padding of product list, will line up */
-  top: calc(135px + 1rem);
+  background-color: white;
+
+  @media (min-width: 860px) {
+    position: sticky;
+    /* Height of Header component plus padding of product list, will line up... but not with mobile as menu layout changes! */
+    top: calc(135px + 1rem);
+  }
 `;
 
 export async function loader() {
