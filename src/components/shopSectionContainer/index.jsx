10fa1f3ce8541813652d5a4f1a7612db02518b5f
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-25 16:13:43 +0100

    ShopSectionContainer: add default background color so I don't have to keep adding it

--- src/components/shopSectionContainer/index.jsx
+++ src/components/shopSectionContainer/index.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+  background-color: white;
   border: 1px solid var(--color--light);
   border-radius: 5px;
   padding: 1.5rem;
