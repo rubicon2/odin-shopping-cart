bbbdff94d700729343c11f8c3799b72811e38946
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-06-26 10:15:01 +0100

    NavBar: implement toggleable mobile menu

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,6 +1,34 @@
+import menuIcon from '/burger-menu.svg';
 import NavBasket from '../navBasket';
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const NavButton = styled.button`
+  background-color: transparent;
+  border: none;
+
+  font-size: 1.3rem;
+  font-weight: 700;
+  text-decoration: none;
+  color: var(--color--dark);
+
+  /* width: max-content; */
+
+  @media (min-width: 860px) {
+    display: none;
+  }
+`;
+
+const MenuIcon = styled.img`
+  height: 45px;
+`;
 
 const NavList = styled.ul`
   list-style: none;
@@ -43,33 +71,55 @@ const NavLinkStyled = styled(NavLink)`
 `;
 
 export default function NavBar() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (!open && window.innerWidth >= 860) setOpen(true);
+      else setOpen(false);
+    };
+
+    addEventListener('resize', handleResize);
+    return () => {
+      removeEventListener('resize', handleResize);
+    };
+  });
+
   return (
-    <nav>
-      <NavList>
-        <li>
-          <NavLinkStyled
-            to="/"
-            className={(isActive, isPending) =>
-              isActive ? 'active' : isPending ? 'pending' : ''
-            }
-          >
-            Home
-          </NavLinkStyled>
-        </li>
-        <li>
-          <NavLinkStyled
-            to="/shop"
-            className={(isActive, isPending) =>
-              isActive ? 'active' : isPending ? 'pending' : ''
-            }
-          >
-            Shop
-          </NavLinkStyled>
-        </li>
-        <li>
-          <NavBasket />
-        </li>
-      </NavList>
-    </nav>
+    <Nav>
+      <NavButton onClick={() => setOpen(!open)}>
+        <MenuIcon
+          src={menuIcon}
+          alt={open ? 'Close nav menu' : 'Open nav menu'}
+        />
+      </NavButton>
+      {open && (
+        <NavList>
+          <li>
+            <NavLinkStyled
+              to="/"
+              className={(isActive, isPending) =>
+                isActive ? 'active' : isPending ? 'pending' : ''
+              }
+            >
+              Home
+            </NavLinkStyled>
+          </li>
+          <li>
+            <NavLinkStyled
+              to="/shop"
+              className={(isActive, isPending) =>
+                isActive ? 'active' : isPending ? 'pending' : ''
+              }
+            >
+              Shop
+            </NavLinkStyled>
+          </li>
+          <li>
+            <NavBasket />
+          </li>
+        </NavList>
+      )}
+    </Nav>
   );
 }
