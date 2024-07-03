9a2005eb8253faa23039072b1ea706f16952b98e
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-07-03 11:02:23 +0100

    Add comment to explain purpose of css property

--- src/components/productList/index.jsx
+++ src/components/productList/index.jsx
@@ -7,6 +7,9 @@ const Container = styled.main`
   /* Ensure rows do not stretch to fill parent container */
   grid-auto-rows: min-content;
   gap: 1.5rem;
+  /* This line ensures the sticky mobile filter sticks to the bottom but does not */
+  /* overlap the last item on the product list like it would with position: fixed */
+  /* Yes, this is not a good way of doing it, at all - but time to finish up this project. */
   min-height: 100vh;
 `;
 
