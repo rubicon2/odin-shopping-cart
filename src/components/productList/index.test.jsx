c14d82a2a007029eefd0dd300aa32ea461cc0a72
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-07-08 14:48:22 +0100

    ProductList: add tests

---
+++ src/components/productList/index.test.jsx
@@ -0,0 +1,43 @@
+import ProductList from '.';
+import ProductCard from '../productCard';
+import { render, screen } from '@testing-library/react';
+import { vi } from 'vitest';
+
+afterEach(() => {
+  vi.resetAllMocks();
+});
+
+afterAll(() => {
+  vi.restoreAllMocks();
+});
+
+vi.mock('../productCard');
+
+const products = [
+  {
+    id: 0,
+  },
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+  {
+    id: 3,
+  },
+];
+
+describe('Product List', () => {
+  it('Renders "No products found!" message if products prop array is empty', () => {
+    render(<ProductList />);
+    expect(ProductCard).toHaveBeenCalledTimes(0);
+    expect(screen.getByText('No products found!')).toBeDefined();
+  });
+
+  it('Renders a product card for each element in the products prop array', () => {
+    render(<ProductList products={products} />);
+    console.log('MEGA: ', ProductCard.mock.calls);
+    expect(ProductCard).toHaveBeenCalledTimes(4);
+  });
+});
